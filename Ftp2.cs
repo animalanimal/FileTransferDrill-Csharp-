19c7a0f2b638b13297c9816ddd8a613653c041af
Joseph Ghilardi <josephghilardi@gmail.com>
2014-11-24 10:10:37 -0800

    File Transfer App
    A simple program to transfer dated files in C#

---
+++ Ftp2.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileTransferApp
+{
+    class Ftp2
+    {
+
+        
+        public void FileTransfer()
+        {
+            string fileName = ".txt";
+            string sourcePath = @"C:\tf\hold";
+            string destPath = @"C:\tf\rec";
+            string sourceFile = Path.Combine(sourcePath, fileName);
+            string destFile = Path.Combine(destPath, fileName);
+
+            if (Directory.Exists(destPath))
+            {
+                Directory.CreateDirectory(destPath);
+            }
+
+            if (Directory.Exists(sourcePath))
+            {
+                
+                string[] files = Directory.GetFiles(sourcePath);
+
+                // Copy the files more than 24hrs old, overwrites existing files
+                foreach (string s in files)
+                {
+                    DateTime fileDate = File.GetCreationTime(s);
+                    bool timeCheck = fileDate > DateTime.Now.AddHours(-24);
+
+                    if (timeCheck == true)
+                    {
+                        fileName = Path.GetFileName(s);
+                        destFile = Path.Combine(destPath, fileName);
+                        File.Copy(s, destFile, true);
+
+                        Console.WriteLine("Transferring file ... ");
+                    }                    
+                }
+                Console.WriteLine("\nFile transfer complete.\n");
+            }
+            else
+            {
+                Console.WriteLine("Source path does not exist.");
+            }
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileTransferApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            Ftp2 Ftp = new Ftp2();
+
+            Ftp.FileTransfer();
+
+            Console.ReadLine();
+        }
+    }
+}
